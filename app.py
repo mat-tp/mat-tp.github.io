a1a3167f276df08d9ceb2bd517f8cd73016c81fc
mat-tp <matlalat53@gmail.com>
2024-12-25 14:09:26 +0200

    Add new portfolio files

---
+++ app.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+# Home route
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+# Projects route
+@app.route("/projects")
+def projects():
+    return render_template("projects.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
