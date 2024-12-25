2705241963043f7d15326286d0ca4fd7969f8321
mat-tp <matlalat53@gmail.com>
2024-12-25 17:17:41 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, send_from_directory
+from flask import Flask, render_template
 
 app = Flask(__name__, static_folder="static", template_folder=".")
 
@@ -11,11 +11,6 @@ def home():
 @app.route("/projects")
 def projects():
     return render_template("projects.html")
-
-# Static files (if needed for serving additional assets dynamically)
-@app.route("/static/<path:filename>")
-def static_files(filename):
-    return send_from_directory("static", filename)
 
 if __name__ == "__main__":
     app.run(debug=True)
