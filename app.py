5658a517b58cdd85b388a24ce0939effebfc867a
mat-tp <matlalt63@gmail.com>
2025-02-06 12:53:16 +0200

    Added new files to the new branch

--- app.py
+++ app.py
@@ -1,16 +1,19 @@
 from flask import Flask, render_template
 
-app = Flask(__name__, static_folder="static", template_folder=".")
+# Create the Flask app with custom static and template folders
+app = Flask(__name__, static_folder="static", template_folder="templates")
 
 # Home route
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("index_flask.html")
 
 # Projects route
 @app.route("/projects")
 def projects():
-    return render_template("projects.html")
+    return render_template("projects_flask.html")
 
 if __name__ == "__main__":
+    # Run the app in debug mode locally
     app.run(debug=True)
+
